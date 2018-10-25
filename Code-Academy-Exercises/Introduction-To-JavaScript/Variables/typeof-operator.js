757f5cc6160b38caeddff95288a4d02793935c95
Gbarra9 <33293205+Gbarra9@users.noreply.github.com>
2018-10-25 17:15:40 -0400

    Minor change to code

--- Code-Academy-Exercises/Introduction-To-JavaScript/Variables/typeof-operator.js
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/typeof-operator.js
@@ -2,5 +2,9 @@ let newVariable = 'Playing around with typeof.';
 
 console.log(typeof newVariable);
 
+newVariable = 1;
+
+console.log(typeof newVariable);
+
 // check data type of value to right using typeof operator
 // typeof returns value's data type via a string
