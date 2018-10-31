46a6300e2f7ecbf9e93a11b6aa3aa9404b2ddbaa
Gbarra9 <33293205+Gbarra9@users.noreply.github.com>
2018-10-31 05:14:11 -0400

    Intermediate JS Modules commit 1

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Intermediate-JavaScript-Modules/Hello-Modules.js
@@ -0,0 +1,11 @@
+
+// JavaScript modules are reusable pieces of code that can be exported from one program and imported for use in another program
+
+/*
+Modules uses:
+-find, fix and easily debug code
+-reuse and recycle defined logic in different parts of application
+-keep information private and protected from other modules
+-prevent pollution of global namespace and potential naming collisions, by cautiously selecting variables and behavior we load into a program
+
+*/
---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Intermediate-JavaScript-Modules/module-exports.js
@@ -0,0 +1,15 @@
+const Airplane = {}
+
+Airplane.myAirplane = "StarJet"
+
+module.exports = Airplane
+// module is variable that represents the module, exports exposes module as an object
+
+/*
+
+How to export modules:
+1) define an object to represent module
+2) Add data or behavior to the module
+3) Export the module
+
+*/
